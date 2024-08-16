0915248cc64c7c4cecc1a664f6a8dfff50270c69
Thomas Eleff <tome18@comcast.net>
2024-08-16 16:20:11 -0500

    Updates assemblit URL

--- assemblit/__init__.py
+++ assemblit/__init__.py
@@ -3,5 +3,5 @@
 🦄 `assemblit` is a Python package that provides a framework of 👑 [streamlit](https://streamlit.io/) based web-components for quickly assembling end-to-end analytics-as-a-service (AaaS) web-applications. `assemblit` comes with user-authentication, a lightweight sqlite3-database backend, and workflow orchestration via 🧊 [prefect](https://www.prefect.io).
 """
 
-_URL = 'https://www.assemblit.org'
+_URL = 'https://assemblit.org'
 _DOCS_URL = '%s/api/assemblit' % (_URL)
